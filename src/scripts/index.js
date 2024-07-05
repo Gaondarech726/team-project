5271a0ca56b481692b59e40497383b34a504dc0d
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-07-05 19:59:31 +0300

    clearing-code-junk

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,72 +1,72 @@
-import postTemplate from '../templates/posts.hbs';
-import postsAPI from './postsAPI.js';
+// import postTemplate from '../templates/posts.hbs';
+// import postsAPI from './postsAPI.js';
 
-const containerPost = document.querySelector('#postsContainer');
+// const containerPost = document.querySelector('#postsContainer');
 
-function renderPosts(posts) {
-  const html = postTemplate({
-    post: posts,
-  });
-  containerPost.innerHTML = html;
-}
+// function renderPosts(posts) {
+//   const html = postTemplate({
+//     post: posts,
+//   });
+//   containerPost.innerHTML = html;
+// }
 
-async function getPosts() {
-  try {
-    const posts = await postsAPI.getAllPosts();
-    return posts;
-  } catch (e) {
-    containerPost.innerHTML = 'Помилка';
-    console.error('getAllPosts', e);
-  }
-}
+// async function getPosts() {
+//   try {
+//     const posts = await postsAPI.getAllPosts();
+//     return posts;
+//   } catch (e) {
+//     containerPost.innerHTML = 'Помилка';
+//     console.error('getAllPosts', e);
+//   }
+// }
 
-function submitHandler(event) {
-  console.log(event);
-  event.preventDefault;
-  if (event.target.id === 'createPostForm') {
-    createNewPost(event.target);
-  }
-}
+// function submitHandler(event) {
+//   console.log(event);
+//   event.preventDefault;
+//   if (event.target.id === 'createPostForm') {
+//     createNewPost(event.target);
+//   }
+// }
 
-function clickHandler(event) {
-  console.log(event);
-  if (event.target.closest('.deletePostButton')) {
-    const id = event.target.dataset.id;
-    console.log(id);
+// function clickHandler(event) {
+//   console.log(event);
+//   if (event.target.closest('.deletePostButton')) {
+//     const id = event.target.dataset.id;
+//     console.log(id);
 
-    deletePostId(id);
-  }
-}
+//     deletePostId(id);
+//   }
+// }
 
-async function updatePostLists() {
-  const posts = await getPosts();
-  renderPosts(posts);
-}
+// async function updatePostLists() {
+//   const posts = await getPosts();
+//   renderPosts(posts);
+// }
 
-async function deletePostId(id) {
-  await postsAPI.deletePost(id);
+// async function deletePostId(id) {
+//   await postsAPI.deletePost(id);
 
-  await updatePostLists();
-}
+//   await updatePostLists();
+// }
 
-async function createNewPost(form) {
-  //отримати данні
-  const title = form.title.value;
-  const content = form.content.value;
+// async function createNewPost(form) {
+//   //отримати данні
+//   const title = form.title.value;
+//   const content = form.content.value;
 
-  //передати данні в API
-  postsAPI.addPost({ title, content });
-  await updatePostLists();
-}
+//   //передати данні в API
+//   postsAPI.addPost({ title, content });
+//   await updatePostLists();
+// }
 
-async function startApp() {
-  const posts = await getPosts();
+// async function startApp() {
+//   const posts = await getPosts();
 
-  await renderPosts(posts);
-}
+//   await renderPosts(posts);
+// }
 
-startApp();
+// startApp();
 
-document.addEventListener('submit', submitHandler);
+// document.addEventListener('submit', submitHandler);
 
-document.addEventListener('click', clickHandler);
+// document.addEventListener('click', clickHandler);
