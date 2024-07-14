7cf69a1ec368c3bc0d61f3e176bb5cd28e1c7162
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-07-14 11:32:05 +0300

    animation enter & backdrop

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -70,3 +70,22 @@
 // document.addEventListener('submit', submitHandler);
 
 // document.addEventListener('click', clickHandler);
+
+const modal = document.querySelector('.modal-window');
+const backdrop = document.querySelector('.backdrop');
+
+function clickHandler(event) {
+  console.log(event);
+  if (event.target.closest('.test-card')) {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  } else if (event.target.closest('.backdrop')) {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+  } else if (event.target.closest('.close')) {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+  }
+}
+
+document.addEventListener('click', clickHandler);
