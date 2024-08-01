3b86045bdc952c0ecefc405129fdb1a274ea7395
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-08-01 16:04:19 +0300

    error

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,9 @@
-import './backdrop.js';
-import './pagination.js'
+import { cardEventListener } from './card';
+import { initFilters } from './filters';
+
+async function init() {
+  await initFilters();
+  cardEventListener();
+}
+
+init();
